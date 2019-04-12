b7eaa9e2a697d1f2e4de602a57c9f7fe0e86cce6
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-04-12 08:42:16 +0200

    Exchange inline with roxygen comment in center.R

--- center.R
+++ center.R
@@ -1,5 +1,15 @@
+#' Center a vector around a specified value
+#' 
+#' Take a *center* value and rearrange data points around it regarding the mean.
+#'
+#' @param data  The single-dimension, numeric data vector to center 
+#' @param center The numeric scalar around which the data shall be arranged
+#'
+#' @return centered_data The centered numeric data set
+#' @export
+#'
+#' @examples center(c(1, 2, 3), 0) # should result in [1] -1  0  1
 center <- function(data, center) {
-  # Take a *center* and rearrange data points around it regarding the mean.
   centered_data <- (data - mean(data) + center)
   return(centered_data)
 }
