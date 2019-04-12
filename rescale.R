c9733e8b9af311c5488f0b430c0ea8f233812fa5
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-04-12 08:39:50 +0200

    Add roxygen docs to script rescale.R

--- rescale.R
+++ rescale.R
@@ -1,3 +1,11 @@
+#' Normalize a data set
+#'
+#' @param v The numeric, single-dimension vector which should be normalized
+#'
+#' @return The normalized version of `v`
+#' @export
+#'
+#' @examples rescale(c(0, 1, 2, 3, 4)) # should result in [1] 0.00 0.25 0.50 0.75 1.00
 rescale <- function(v) {
   L <- min(v)
   H <- max(v)
