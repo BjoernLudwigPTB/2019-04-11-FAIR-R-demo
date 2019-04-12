5db32c6b7546826378b5276f0c0e0a081ed7c510
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-04-12 08:16:49 +0200

    Add an informal comment to script center.R

--- center.R
+++ center.R
@@ -1,4 +1,5 @@
 center <- function(data, center) {
+  # Take a *center* and rearrange data points around it regarding the mean.
   centered_data <- (data - mean(data) + center)
   return(centered_data)
 }
